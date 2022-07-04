46a94072cacf31dd5cad7a6202b232578dcdfbd2
rebreather <yds04116@gmail.com>
2022-07-04 18:20:52 +0900

    Auth상태, Board 상태를 스토어로 관리(저장)

--- kanban-app/src/store/index.js
+++ kanban-app/src/store/index.js
@@ -7,7 +7,18 @@ import mutations from './mutations'
 
 Vue.use(Vuex)
 
+const state = {
+    auth: { //상태 'Auth'
+        token: null, //token을 null로 초기화
+        userId: null //'userId'를 null로 초기화
+    },
+    board: {
+        lists: [] // 상태 TaskList는 빈 배열로 초기화
+    }
+}
+
 export default new Vuex.Store({
+    state,
     getters,
     actions,
     mutations,
