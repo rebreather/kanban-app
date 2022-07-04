b75af6a2c8dd2102c643866e543b42ae3ba2b14f
rebreather <yds04116@gmail.com>
2022-07-04 16:33:54 +0900

    vuex와 promise 폴리필 등록하기

--- kanban-app/src/main.js
+++ kanban-app/src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
+import 'es6-promise/auto' //promise 폴리필
 import App from './App'
 import router from './router'
+import store from './store' //vuex 스토어 인스턴스 임포트
 
-Vue.config.productionTip = false
+Vue.config.productionTip = process.env.NODE_ENV === 'production' 
+                            //false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store, //임포트한 스토어 인스턴스를 'store' 옵션으로 지정
   render: h => h(App)
 })
