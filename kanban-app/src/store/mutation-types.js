61f71d23b6a46c480975bf1cb5c3f71c4b8e4075
rebreather <yds04116@gmail.com>
2022-07-04 22:55:49 +0900

    데이터플로 설계에서 도출된 뮤테이션 종류 정의

---
+++ kanban-app/src/store/mutation-types.js
@@ -0,0 +1,6 @@
+export const AUTH_LOGIN = 'AUTH_LOGIN' //사용자 로그인
+export const FETCH_ALL_TASKLIST = 'FETCH_ALL_TASKLIST' // 모든 태스크 목록의 정보 받아옴
+export const ADD_TASK = 'ADD_TASK' //태스크 추가
+export const UPDATE_TASK = 'UPDATE_TASK' //태스크 수정
+export const REMOVE_TASK = 'REMOVE_TASK' //태스크 삭제
+export const AUTH_LOGOUT = 'AUTH_LOGOUT' //사용자 로그아웃
