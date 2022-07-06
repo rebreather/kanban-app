2e7692615b8d686e6241dad8c40fd187f20df896
rebreather <yds04116@gmail.com>
2022-07-06 16:58:58 +0900

    main.js 수정, Cannot read properties of undefined (reading 'dispatch') 오류 해결

--- kanban-app/src/main.js
+++ kanban-app/src/main.js
@@ -3,15 +3,29 @@
 import Vue from 'vue'
 import 'es6-promise/auto' //promise 폴리필
 import App from './App'
-import router from './router'
-import store from './store' //vuex 스토어 인스턴스 임포트
+import ErrorBoundary from './ErrorBoundary.vue' // 오류를 포착하는 컴포넌트
+import router from './router/index.js'
+import store from './store/index.js' //vuex 스토어 인스턴스 임포트
 
-Vue.config.productionTip = process.env.NODE_ENV === 'production'
+Vue.config.productionTip = false
+Vue.config.performance = true // NODE_ENV == 'development'로 설정하여 프로파일링 활성화
+
+// ErrorBoundary 컴포넌트 설치
+Vue.component(ErrorBoundary.name, ErrorBoundary)
+
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error('errorHandler err:', err)
+  console.error('errorHandler vm:', vm)
+  console.error('errorHandler info:', info)
+}
+
+// 오류 원인 해결!!
+Vue.prototype.$store = store
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  store, //임포트한 스토어 인스턴스를 'store' 옵션으로 지정
+  store, // 임포트한 스토어 인스턴스를 'store' 옵션으로 전달
   render: h => h(App)
 })
