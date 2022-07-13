c0c0c6133e9646c2666065a6807152a2347a6c00
rebreather <yds04116@gmail.com>
2022-07-13 23:42:24 +0900

    router

--- kanban-app/src/router/routes.js
+++ kanban-app/src/router/routes.js
@@ -3,17 +3,19 @@ import KbnLoginView from '@/components/templates/KbnLoginView.vue'
 import KbnTaskDetailModal from '@/components/templates/KbnTaskDetailModal.vue'
 
 export default [{
-    path:'/',
-    component: KbnBoardView,
-    meta: { requiresAuth: true}
-}, {
-    path:'/login',
-    component: KbnLoginView
-}, {
-    path:'/tasks/:id',
+  path: '/',
+  component: KbnBoardView,
+  meta: { requiresAuth: true },
+  children: [{
+    path: 'tasks/:id',
     component: KbnTaskDetailModal,
-    meta: {requiresAuth: true}
+    name: 'taskDetailModal',
+    meta: { requiresAuth: true }
+  }]
 }, {
-    path:'*',
-    redirect: '/'
+  path: '/login',
+  component: KbnLoginView
+}, {
+  path: '*',
+  redirect: '/'
 }]
